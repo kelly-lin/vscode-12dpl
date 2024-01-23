93924587a457a1d127d371bfa02d1d0120741706
Kelly Lin <findkellylin@gmail.com>
2024-01-23 21:33:47 +1100

    feat: set config

--- src/extension.ts
+++ src/extension.ts
@@ -8,18 +8,22 @@ import {
 
 let client: LanguageClient;
 
-// TODO: move this to config.
-const DEV_CMD =
-  "/Users/kelly.lin/Repos/repoman/github.com/kelly-lin/12d-lang-server/12dls";
-
 export function activate(_context: ExtensionContext) {
+  const config = workspace.getConfiguration("12dpl");
+  const binPath = config.get<string>("languageServerBinPath") ?? "";
+  const includesDir = config.get<string>("includesDir") ?? "";
+  const enableLogging = config.get<boolean>("enableLogging") ?? false;
+  const args = [];
+  if (enableLogging) args.push("-d");
+  if (includesDir) args.push("-i", includesDir);
   const serverOptions: ServerOptions = {
     run: {
-      command: "12d-lang-server",
+      command: binPath,
+      args,
     },
     debug: {
-      command: DEV_CMD,
-      args: ["-d", "-i", "/Users/kelly.lin/Repos/repoman/github.com/kelly-lin/12d-lang-server/lang/includes"],
+      command: binPath,
+      args,
     },
   };
   // Options to control the language client
@@ -32,7 +36,7 @@ export function activate(_context: ExtensionContext) {
     },
   };
   client = new LanguageClient(
-    "12d-lang-server",
+    "12dls",
     "12d Language Server",
     serverOptions,
     clientOptions
