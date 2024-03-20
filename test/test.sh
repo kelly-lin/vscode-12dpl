94c10f9368a22532f67d06f41b6e0122b771e6e9
Kelly Lin <findkellylin@gmail.com>
2024-03-20 21:47:57 +1100

    test: add test script

---
+++ test/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Tests to see if the server version can be fetched.
+#
+# Not the most magnificent test script ever written but has zero dependencies
+# and is very simple and effective.
+
+set -euo pipefail
+
+version=$(grep 'const SERVER_VERSION' ./src/extension.ts | awk -F'=' '{ print $2 }' | sed 's/"\(.*\)";/\1/' | tr -d '[:space:]')
+curl -fIs "https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe" >/dev/null
