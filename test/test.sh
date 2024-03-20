9058a137d10dd260c4e2549b22fb98aa36687535
Kelly Lin <findkellylin@gmail.com>
2024-03-20 21:54:19 +1100

    test: add messaging

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,11 @@
 # Not the most magnificent test script ever written but has zero dependencies
 # and is very simple and effective.
 
-set -euox pipefail
+set -uo pipefail
 
 version=$(grep 'const SERVER_VERSION' ./src/extension.ts | awk -F'=' '{ print $2 }' | sed 's/"\(.*\)";/\1/' | tr -d '[:space:]')
-curl -fIs "https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe" >/dev/null
+url="https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe"
+if ! curl -fIs "https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe" >/dev/null; then
+    echo "could not fetch language server at $url"
+    exit 1
+fi
