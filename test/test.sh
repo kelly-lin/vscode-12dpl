2900cf76efc5285d8e012878a127ff5179948d2b
Kelly Lin <findkellylin@gmail.com>
2024-03-20 21:51:06 +1100

    test: add x opt

--- test/test.sh
+++ test/test.sh
@@ -5,7 +5,7 @@
 # Not the most magnificent test script ever written but has zero dependencies
 # and is very simple and effective.
 
-set -euo pipefail
+set -euox pipefail
 
 version=$(grep 'const SERVER_VERSION' ./src/extension.ts | awk -F'=' '{ print $2 }' | sed 's/"\(.*\)";/\1/' | tr -d '[:space:]')
 curl -fIs "https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe" >/dev/null
