7f190ba667a84dcacf9afdda48b7c6070dee1db0
Kelly Lin <findkellylin@gmail.com>
2024-12-02 20:46:33 +1100

    chore: use dist repo for bin download

--- src/extension.ts
+++ src/extension.ts
@@ -11,7 +11,7 @@ import {
 import { execSync } from "child_process";
 
 const SERVER_VERSION = "v0.1.0-beta.4";
-const SERVER_DOWNLOAD_URL = `https://github.com/kelly-lin/12d-lang-server/releases/download/${SERVER_VERSION}/12dls.exe`;
+const SERVER_DOWNLOAD_URL = `https://github.com/kelly-lin/12d-lang-server-dist/releases/download/${SERVER_VERSION}/12dls.exe`;
 
 let client: LanguageClient;
 
--- test/test.sh
+++ test/test.sh
@@ -8,8 +8,8 @@
 set -uo pipefail
 
 version=$(grep 'const SERVER_VERSION' ./src/extension.ts | awk -F'=' '{ print $2 }' | sed 's/"\(.*\)";/\1/' | tr -d '[:space:]')
-url="https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe"
-if ! curl -fIs "https://github.com/kelly-lin/12d-lang-server/releases/download/$version/12dls.exe" >/dev/null; then
+url="https://github.com/kelly-lin/12d-lang-server-dist/releases/download/$version/12dls.exe"
+if ! curl -fIs "https://github.com/kelly-lin/12d-lang-server-dist/releases/download/$version/12dls.exe" >/dev/null; then
     echo "could not fetch language server at $url"
     exit 1
 fi
